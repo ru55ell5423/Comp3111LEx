be5bcd6efbd29860d30b78b99150336a6fb243ca
Administrator <yacaicd@connect.ust.hk>
2025-09-19 21:01:45 +0800

    Another one

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -11,6 +11,7 @@ public class mainApp1 {
         System.out.println("1 + 1 = "+2);
         System.out.println("New Message");
         System.out.println("One more New Message");
+        System.out.println("Another one");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
